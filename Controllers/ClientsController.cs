5ec7129de746eb3041664c4bc14df57e2589ae4c
grzegorzbarnas <barnasgrzesiek@gmail.com>
2020-01-19 18:04:47 +0100

    initial commit

---
+++ Controllers/ClientsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projekt.Controllers
+{
+    public class ClientsController:Controller
+    {
+        private readonly IClientService clientService;
+
+        public ClientsController(IClientService clientService)
+        {
+            this.clientService = clientService;
+        }
+
+        
+
+        [HttpGet]
+        public async Task<IActionResult> Index() 
+        {
+            var clients = clientService.GetClients();
+            return Ok(clients);
+        }
+
+            
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BazaDanych.DataAccesLayer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Services.Service.Implementation;
+using Services.Service.Interface;
+
+namespace Projekt
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        { 
+
+            services.AddDbContext<ApplicationDB>(options =>
+        options.UseSqlServer(Configuration.GetConnectionString("BloggingDatabase")));
+
+            services.AddTransient<IClientService, ClientServer>();
+
+            services.AddMvc();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            
+        }
+    }
+}
